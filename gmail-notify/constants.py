94212f7f14f395600669fad7ab58e237168568ab
kdrobnyh <klim.drobnyh@gmail.com>
2013-08-31 19:20:40 +0400

    Basic refactoring

--- gmail-notify/constants.py
+++ gmail-notify/constants.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python2
+#!/usr/bin/python2
 # -*- coding: utf-8 -*-
+
 
 class Constants:
     def get_ok(self):
         return 0
+
     def get_connectionerror(self):
         return 1
+
     def get_nologin(self):
         return 2
--- gmail-notify/xmllangs.py
+++ gmail-notify/xmllangs.py
@@ -34,85 +34,93 @@
 from xml.sax.handler import ContentHandler
 from xml import sax
 
+
 # A string description
 class String:
-	def __init__( self, id):
-		self.id = id
-		self.string=str()
+    def __init__(self, id):
+        self.id = id
+        self.string = str()
 
-	# Accessors an mutators
-	def set_string(self, string):
-		self.string = string
+    # Accessors an mutators
+    def set_string(self, string):
+        self.string = string
 
-	def get_string( self):
-		return self.string
+    def get_string(self):
+        return self.string
 
-	def set_id( self, id):
-		self.id = id
+    def set_id(self, id):
+        self.id = id
 
-	def get_id(self):
-		return self.id
+    def get_id(self):
+        return self.id
+
 
 # A language description
 class Lang:
-	def __init__(self, name):
-		self.name=name
-		self.strings=list()
+    def __init__(self, name):
+        self.name = name
+        self.strings = list()
 
-	# Adds a string to this language
-	def add_string(self, string):
-		self.strings.append( string)
-	# Returns a string with the name of the language
-	def get_name(self):
-		return self.name
-	# Returns the string with id=='id'
-	def get_string(self, id):
-		for s in self.strings:
-			if s.id==str(id):
-				return s.get_string()
-		return "(empty)"
+    # Adds a string to this language
+    def add_string(self, string):
+        self.strings.append(string)
+    # Returns a string with the name of the language
+
+    def get_name(self):
+        return self.name
+
+    # Returns the string with id=='id'
+    def get_string(self, id):
+        for s in self.strings:
+            if s.id == str(id):
+                return s.get_string()
+        return "(empty)"
+
 
 # Language XML file parser
 class LangHandler(ContentHandler):
-	def __init__(self):
-		self.langs=list()
-	def startElement( self, name, attrs):
-		if name=="lang":
-			temp_lang = Lang(attrs.getValue("name")) 
-			self.langs.append(temp_lang)
-		if name=="string":
-			s = String( attrs.getValue("id"))
-			temp_lang = self.langs.pop()
-			temp_lang.add_string(s)
-			self.langs.append( temp_lang)
-	
-	def endElement( self, name):
-		return
+    def __init__(self):
+        self.langs = list()
 
-	def characters( self, content):
-		if content.strip()!="":
-			l = self.langs.pop()
-			s = l.strings.pop()
-			s.set_string(content)
-			l.strings.append(s)
-			self.langs.append(l)
+    def startElement(self, name, attrs):
+        if name == "lang":
+            temp_lang = Lang(attrs.getValue("name"))
+            self.langs.append(temp_lang)
+        if name == "string":
+            s = String(attrs.getValue("id"))
+            temp_lang = self.langs.pop()
+            temp_lang.add_string(s)
+            self.langs.append(temp_lang)
+
+    def endElement(self, name):
+        return
+
+    def characters(self, content):
+        if content.strip() != "":
+            l = self.langs.pop()
+            s = l.strings.pop()
+            s.set_string(content)
+            l.strings.append(s)
+            self.langs.append(l)
+
 
 # The main class
 class LangsParser:
-	def __init__(self, filename):
-		self.lh = LangHandler()
-		try:
-			sax.parse( filename, self.lh)
-			print "xmllangs: XML file succesfully parsed"
-		except:
-			print "xmllangs: Error parsing XML file."
-	# Returns a list of all 'Lang' objects
-	def get_all_langs( self):
-		return self.lh.langs
-	
-	# Returns a 'Lang' class object where Lang.name==langname
-	def find_lang(self, langname):
-		for one_lang in self.lh.langs:
-			if one_lang.get_name()==langname:
-				return one_lang
-		return self.lh.langs[0]
+    def __init__(self, filename):
+        self.lh = LangHandler()
+        try:
+            sax.parse(filename, self.lh)
+            print "xmllangs: XML file succesfully parsed"
+        except:
+            print "xmllangs: Error parsing XML file."
+
+    # Returns a list of all 'Lang' objects
+    def get_all_langs(self):
+        return self.lh.langs
+
+    # Returns a 'Lang' class object where Lang.name==langname
+    def find_lang(self, langname):
+        for one_lang in self.lh.langs:
+            if one_lang.get_name() == langname:
+                return one_lang
+        return self.lh.langs[0]
